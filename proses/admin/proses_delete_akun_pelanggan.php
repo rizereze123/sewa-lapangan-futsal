d885dc1f34d3ac0a01f4d374b7dfc4b91b343ce0
dhimaz purnama <dhimazpurnama123@gmail.com>
2023-12-28 11:09:20 +0700

    v0.3

--- proses/admin/proses_delete_akun_pelanggan.php
+++ proses/admin/proses_delete_akun_pelanggan.php
@@ -14,7 +14,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     } else {
         // Handle kesalahan jika gagal menghapus
-        echo "Terjadi kesalahan saat menghapus data akun pelanggan: " . mysqli_error($koneksi);
+        // echo "Terjadi kesalahan saat menghapus data akun pelanggan: " . mysqli_error($koneksi);
+        echo '<html>';
+        echo '<head>';
+        echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
+        echo '</head>';
+        echo '<body>';
+        echo '<script>';
+        echo 'Swal.fire({';
+        echo '  icon: "error",';
+        echo '  title: "Akun ini Sedang Aktif",';
+        echo '}).then(function() {';
+        echo '  window.location.href = "../../admin/manajemen_akun_pelanggan.php";';
+        echo '});';
+        echo '</script>';
+        echo '</body>';
+        echo '</html>';
     }
 } else {
     // Redirect jika akses langsung ke file ini tanpa POST request
--- proses/pelanggan/proses_signup_pelanggan.php
+++ proses/pelanggan/proses_signup_pelanggan.php
@@ -8,25 +8,32 @@ $nama_lengkap = $_POST['nama_lengkap'];
 $telepon = $_POST['telepon'];
 $password = $_POST['password'];
 $encryptedPassword = base64_encode($password);
+// Check if the email is already used
+$queryCheckEmail = "SELECT * FROM pelanggan WHERE email='$email'";
+$resultCheckEmail = mysqli_query($koneksi, $queryCheckEmail);
 
-// Contoh validasi sederhana (Anda dapat menambahkan validasi lebih lanjut)
-if (empty($email) || empty($nama_lengkap) || empty($telepon) || empty($password)) {
-    // Handle kesalahan jika data tidak lengkap
-    echo "Data akun pelanggan tidak lengkap. Kembali <a href='../../index.php'>ke halaman dashboard</a>.";
+if (mysqli_num_rows($resultCheckEmail) > 0) {
+    // Handle error if email is already used
+    echo "Email yang anda daftarkan sudah terdaftar. Kembali <a href='../../index.php'>ke halaman dashboard</a>.";
 } else {
-    // Query SQL untuk menambahkan data lapangan ke database
-    $query = "INSERT INTO pelanggan (email, nama_lengkap, telepon, password, tanggal_daftar) 
-              VALUES ('$email', '$nama_lengkap', '$telepon', '$encryptedPassword', NOW())";
-
-    if (mysqli_query($koneksi, $query)) {
-        // Data berhasil ditambahkan
-        header("Location: ../../signup_pelanggan.php?pesan=success"); // Redirect kembali ke halaman login
+    // Continue with the original code
+    if (empty($email) || empty($nama_lengkap) || empty($telepon) || empty($password)) {
+        // Handle kesalahan jika data tidak lengkap
+        echo "Data akun pelanggan tidak lengkap. Kembali <a href='../../index.php'>ke halaman dashboard</a>.";
     } else {
-        // Handle kesalahan jika query gagal
-        // echo "Terjadi kesalahan. Kembali <a href='../../index.php'>ke halaman dashboard</a>.";
-        header("Location: ../../signup_pelanggan.php?pesan=fail");
-    }
+        $query = "INSERT INTO pelanggan (email, nama_lengkap, telepon, password, tanggal_daftar) 
+                 VALUES ('$email', '$nama_lengkap', '$telepon', '$encryptedPassword', NOW())";
 
-    // Tutup koneksi ke database
-    mysqli_close($koneksi);
+        if (mysqli_query($koneksi, $query)) {
+            // Data berhasil ditambahkan
+            header("Location: ../../signup_pelanggan.php?pesan=success"); // Redirect kembali ke halaman login
+        } else {
+            // Handle kesalahan jika query gagal
+            // echo "Terjadi kesalahan. Kembali <a href='../../index.php'>ke halaman dashboard</a>.";
+            header("Location: ../../signup_pelanggan.php?pesan=fail");
+        }
+
+        // Tutup koneksi ke database
+        mysqli_close($koneksi);
+    }
 }
